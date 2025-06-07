00735b473b6a20a11c2db2a1b5788498e55467a8
Kee Seung Jun <keesj7@naver.com>
2025-06-07 23:10:40 +0900

    디버깅 및 매핑 0523 22:43 GPT API연결 중 0525 21:41      23:25 0526 02:45 0526 17:47 0531 22:47 스키마 만들기 직전(register) 0601 00:19 회원가입 필수조건 주석처리 후 DB접근 확인 0601 23:55 userhealth 압축 도중 0602 16:49 과외 이후 0603 0606 23:55 쿠키설정중 0607 23:10

--- src/main/java/com/seonier/service/ExampleService.java
+++ src/main/java/com/seonier/service/ExampleService.java
@@ -49,17 +49,17 @@ public class ExampleService {
 				.temperature(0.0) // 무작위성" 또는 "창의성"을 조절하는 파라미터
 				.responseFormat(ResponseFormat.builder().type(ResponseFormat.Type.TEXT).build()); // 응답을 어떻게 받을지 정의
 
-		// TODO 실제 OpenAI 호출하는 부분
-		// ChatResponse chatResponse = this.openAiChatModel.call(
-		// 		new Prompt(UserMessage.builder().text(content).build(), builder.build())
-		// );
-		// String result = chatResponse.getResult().getOutput().getText();
-		// log.debug("Open AI Text: {}", result);
-
-		// 응답받은 값을 정의한다.
-		// { name: "청소 업무", salary: "220만원", location: "대전 서구", company: "클린업", contact: "042-123-4567" },
-		// { name: "배달 보조", salary: "200만원", location: "대전 중구", company: "퀵익스프레스", contact: "010-2222-3333" },
-		// { name: "식당 서빙", salary: "230만원", location: "대전 유성구", company: "맛집식당", contact: "042-987-6543" },
+//		 TODO 실제 OpenAI 호출하는 부분
+//		 ChatResponse chatResponse = this.openAiChatModel.call(
+//		 		new Prompt(UserMessage.builder().text(content).build(), builder.build())
+//		 );
+//		 String result = chatResponse.getResult().getOutput().getText();
+//		 log.debug("Open AI Text: {}", result);
+//
+//		 응답받은 값을 정의한다.
+//		 { name: "청소 업무", salary: "220만원", location: "대전 서구", company: "클린업", contact: "042-123-4567" },
+//		 { name: "배달 보조", salary: "200만원", location: "대전 중구", company: "퀵익스프레스", contact: "010-2222-3333" },
+//		 { name: "식당 서빙", salary: "230만원", location: "대전 유성구", company: "맛집식당", contact: "042-987-6543" },
 
 		// TODO 이곳에 실제 데이터가 들어가야한다. 이 부분은 임의적으로 만든 것임.
 		List<Map<String, String>> list = List.of(
